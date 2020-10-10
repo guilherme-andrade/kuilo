b44ae582da39c7299a2856678a6be9504afe62ee
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-10-10 05:50:38 +0200

    fixed redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
       expire_after: 1.year,
       ttl: 1.year,
       key_prefix: "app:session:",
-      url: ENV.fetch("HEROKU_REDIS_MAROON_URL"),
+      url: ENV.fetch("REDIS_URL"),
     }
   }
 
